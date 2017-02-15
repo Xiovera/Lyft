15e5e8f3c97ca5c52ef4a5a380289912b665f631
Rocio Yovera <rocioyovera@ gmail.com>
2017-02-15 13:19:17 -0500

    agregué map

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 function init () {
 	
 }
+//para definir lat y lng
+
+var miMap;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -16.45738919999998, lng: -71.5315308},
+    zoom: 14
+  });
+}
+//para marcadores
+
+var myLatLng = {lat: -16.45738919999998, lng: -71.5315308};
+
+var pazPeru = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+    });
